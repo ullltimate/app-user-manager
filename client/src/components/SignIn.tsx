175f5e4fbb25312b6ae531670e4164bee1468ee3
ullltimate <ullltimate.zp@gmail.com>
2023-08-04 15:33:06 +0300

    feat: add sign up and sign in layout

---
+++ client/src/components/SignIn.tsx
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function SignIn() {
+  return (
+    <>
+        <Form className='p-3'>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Sign In
+          </Button>
+        </Form>
+    </>
+  )
+}
+
+export default SignIn
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+//import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from './components/ErrorPage.tsx';
+import SignUp from './components/SignUp.tsx';
+import SignIn from './components/SignIn.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <SignUp />,
+      },
+      {
+        path: "/signIn",
+        element: <SignIn />,
+      },
+    ],
   },
 ]);
 
