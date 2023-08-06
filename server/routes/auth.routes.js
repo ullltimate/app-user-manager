a3bfe8c8bee2fadef365cbd2f7aed22032363935
ullltimate <ullltimate.zp@gmail.com>
2023-08-06 15:50:31 +0300

    feat: POST login with update signIn

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const Router = require("express");
 const User = require("../models/User");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require("config");
 const {check, validationResult} = require('express-validator');
 const router = new Router();
 
@@ -24,7 +26,7 @@ router.post('/registration',
             if(candidate){
                 return res.status(400).json({message: `User with email ${email} already exist`})
             }
-            const hashPassword = await bcrypt.hash(password, 15)
+            const hashPassword = await bcrypt.hash(password, 8)
             const user = new User({name, email, password: hashPassword});
             await user.save();
             return res.json({message: 'User was created'})
@@ -34,5 +36,35 @@ router.post('/registration',
             res.send({message: 'Server error'});
         }
     })
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        if (!user){
+            return res.status(404).json({message: "User with this email not found"});
+        }
+        const isPassValid = bcrypt.compareSync(password, user.password)
+        if(!isPassValid){
+            return res.status(400).json({message: "Invalid password"});
+        }
+        await User.updateOne({email}, {$set: {signIn: Date.now()}});
+        const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn:"1h"});
+        return res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                signUp: user.signUp,
+                signIn: user.signIn,
+                status: user.status,
+            }
+        })
+    } catch (e) {
+        console.log(e);
+        res.send({message: 'Server error'});
+    }
+})
 
 module.exports = router
