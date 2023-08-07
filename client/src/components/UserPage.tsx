c761a3fe0e14a8db7aaf61824ce235b5b1a704eb
ullltimate <ullltimate.zp@gmail.com>
2023-08-08 02:17:55 +0300

    feat: add get all users

--- client/src/components/UserPage.tsx
+++ client/src/components/UserPage.tsx
@@ -2,12 +2,21 @@ import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
-import users from './helpUsers';
 import User from './User';
 import Header from './Navbar';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { getUsers } from '../action/getUsers';
 
 function Users() {
+  const [users, setUsers] = useState<any>(null);
+  const [isLoader, setLoader] = useState(true);
+
+  useEffect(() => {
+    getUsers(setUsers, setLoader);
+  },[])
+  console.log(users)
+
   return (
     <> 
       <Header />
@@ -34,7 +43,17 @@ function Users() {
               </tr>
             </thead>
             <tbody>
-              {users.map(el => <User key={el.id} id={el.id} name={el.name} email={el.email} dateSignUp={el.singUpdate} dateSignIn={el.singIndate} status={el.status}/>)}
+            { 
+              isLoader 
+              ? 
+              <tr><td colSpan={7}>
+                <div className="text-center">
+                  <Spinner className='m-5' animation="border" variant="secondary" />
+                </div>
+              </td></tr>
+              :
+              users.map((el: any) => <User key={el._id} id={el._id} name={el.name} email={el.email} dateSignUp={el.signUp} dateSignIn={el.signIn} status={el.status}/>)
+            }
             </tbody>
         </Table>
       </Container>
