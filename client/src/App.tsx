791dc8a7e195b0ecd00f6dbe3c271f326577c068
ullltimate <ullltimate.zp@gmail.com>
2023-08-09 01:45:18 +0300

    fix: fix adaptive layout table

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Navbar';
 import SignUp from './components/SignUp';
+import './App.css';
 
 function App() {
   return (
